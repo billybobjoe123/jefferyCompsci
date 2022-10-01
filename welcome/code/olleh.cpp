b94f17fc3ccf96c0b3a3e0efcd9a85753229cf3c
root <root@LAPTOP-QDIIB904.localdomain>
2022-10-01 14:16:04 -0700

    first commit

---
+++ welcome/code/olleh.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h> 
+using namespace std;
+
+string flip(string input) {
+    int currentSp = 0;
+    int prevSp = 0;
+    string result = "";
+    for(int i = 0;i<input.length();i++) {
+        if (ispunct(input[i])!=0 || input[i] == ' ') { 
+            prevSp = currentSp;
+            currentSp = i;
+            string str = input.substr(prevSp, currentSp);
+            
+            reverse(str.begin(), str.end());
+            result += str;
+        }
+    }
+    prevSp = currentSp;
+    currentSp = input.length()-1;
+    string str = input.substr(prevSp, currentSp);
+    result += str;
+    cout<<result<<endl;
+    return result;
+}
+
+
+
+int main() {
+    cout<<"input a string"<<endl;
+    string input;
+    getline(cin,input);
+    string result = flip(input);
+    cout<<result<<endl;
+}
