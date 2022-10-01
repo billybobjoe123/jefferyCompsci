bad7e84bbaabdd6fa0ddf18b81c4712f0764f0bc
jeffery wang <jeffery.wang.ca@gmail.com>
2022-10-01 14:36:41 -0700

    bugfix

--- welcome/code/olleh.cpp
+++ welcome/code/olleh.cpp
@@ -7,7 +7,7 @@ string flip(string input) {
     int currentSp = 0;
     int prevSp = 0;
     string result = "";
-    for(int i = 0;i<input.length();i++) {
+    for(int i = 0;i<input.size();i++) {
         if (ispunct(input[i])!=0 || input[i] == ' ') { 
             prevSp = currentSp;
             currentSp = i;
