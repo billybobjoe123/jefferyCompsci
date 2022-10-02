bda381541fe938edb368fbb33c7d87761e9ec266
jeffery wang <jeffery.wang.ca@gmail.com>
2022-10-01 21:16:30 -0700

    FINALLY! FUCK substr() for having second parameter be length of substring instead of end index. can finally stop bashing my head against the table.

--- welcome/code/olleh.cpp
+++ welcome/code/olleh.cpp
@@ -3,48 +3,56 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
-string flip(string input) {
-    int currentSp = 0;
-    int prevSp = -1;
+string flip(string input, vector<int> ind) {
+    int size = ind.size();
+    int length = input.length();
     string result = "";
-    int isSec = 0;
-    int size = input.length();
-    for(int i = 0;i<size;i++) {
-        if (ispunct(input[i])!=0 || input[i] == ' ') { 
-            prevSp = currentSp;
-            currentSp = i;
-            if (isSec==1) {
-                prevSp++;
-            }
-            string str = input.substr(prevSp, currentSp);
-            isSec = 1;
-            cout<<str<<endl;
-            reverse(str.begin(), str.end());
-            result += str + input[i];
-            cout<<result<<endl;
-        }
-    }
-    cout<<result<<endl;
-    prevSp = currentSp;
-    currentSp = size-1;
-    string str;
-    if (currentSp != prevSp) {
-        str = input.substr(prevSp, size);
+    if (size == 0) {
+        string temp = input;
+        reverse(temp.begin(),temp.end());
+        return temp;
     }
     else {
-        str = "";
+        string temp = input.substr(0,ind[0]);
+        reverse(temp.begin(),temp.end());
+        result += temp + input[ind[0]];
     }
-    reverse(str.begin(), str.end());
-    result += str;
-    //cout<<result<<endl;
+
+    for (int i = 1; i<size;i++) {
+        if(ind[i]-ind[i-1]==1) {
+            result += input[ind[i]];
+        }
+        else {
+            string temp = input.substr(ind[i-1]+1,ind[i]-(ind[i-1]+1));
+            reverse(temp.begin(),temp.end());
+            result += temp + input[ind[i]];
+        }
+    }
+    string temp = input.substr(ind[size-1]+1,length-(ind[size-1]+1));
+    reverse(temp.begin(),temp.end());
+    result += temp;
+
+    
     return result;
 }
 
-
+vector<int> findSpaceAndPunct(string input) {
+    vector<int> ind;
+    int size = input.length();
+    for(int i = 0;i<size;i++) {
+        if (ispunct(input[i])!=0 || input[i] == ' ') { 
+            ind.push_back(i);
+        }
+    }
+    return ind;
+}
 
 int main() {
     string input;
     getline(cin,input);
-    string result = flip(input);
+    vector<int> ind = findSpaceAndPunct(input);
+    
+    string result = flip(input,ind);
+    
     cout<<result<<endl;
 }
