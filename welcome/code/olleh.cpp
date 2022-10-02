29f44be813d6e3d01a630dc59bd9153a95ca6a15
jeffery wang <jeffery.wang.ca@gmail.com>
2022-10-01 17:09:36 -0700

    idfk man

--- welcome/code/olleh.cpp
+++ welcome/code/olleh.cpp
@@ -5,31 +5,44 @@ using namespace std;
 
 string flip(string input) {
     int currentSp = 0;
-    int prevSp = 0;
+    int prevSp = -1;
     string result = "";
+    int isSec = 0;
     int size = input.length();
     for(int i = 0;i<size;i++) {
         if (ispunct(input[i])!=0 || input[i] == ' ') { 
             prevSp = currentSp;
             currentSp = i;
+            if (isSec==1) {
+                prevSp++;
+            }
             string str = input.substr(prevSp, currentSp);
-            
+            isSec = 1;
+            cout<<str<<endl;
             reverse(str.begin(), str.end());
-            result += str;
+            result += str + input[i];
+            cout<<result<<endl;
         }
     }
+    cout<<result<<endl;
     prevSp = currentSp;
     currentSp = size-1;
-    string str = input.substr(prevSp, currentSp);
+    string str;
+    if (currentSp != prevSp) {
+        str = input.substr(prevSp, size);
+    }
+    else {
+        str = "";
+    }
+    reverse(str.begin(), str.end());
     result += str;
-    cout<<result<<endl;
+    //cout<<result<<endl;
     return result;
 }
 
 
 
 int main() {
-    cout<<"input a string"<<endl;
     string input;
     getline(cin,input);
     string result = flip(input);
