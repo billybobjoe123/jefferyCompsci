047ba337095cc9d4cdd819488008aaa0d95c4324
jeffery wang <jeffery.wang.ca@gmail.com>
2022-10-16 01:08:34 -0700

    fixes

--- assort/List.cpp
+++ assort/List.cpp
@@ -1,4 +1,6 @@
 #include "List.h"
+#include <stdexcept>
+#include <iostream>
 List::List() {
     head = NULL;
 } 
@@ -46,20 +48,154 @@ size_t List::count() const {
     return 0;
 }
 void List::insert(const std::string& value) {
+    if (head == NULL) {
+        head = new Node;
+        head->data = value;
+        return;
+    }
+    if (value < head->data) {
+        Node* temp = head;
+        head = new Node;
+        head->next = temp;
+        head->data = value;
+        return;
+    }
+    Node* curr = head;
+    Node* next = head->next;
+    while (true) {
+        if (next == NULL) {
+            curr->next = new Node;
+            curr->next->data = value;
+            curr->next->next = NULL;
+        }
+        else {
+            if (curr->data < value && next->data > value) {
+                Node* temp = next;
+                curr->next = new Node;
+                curr->next->next = temp;
+                curr->next->data = value;
+                return;
+            }
+            else {
+                curr = next;
+                next = curr->next;
+            }
+        }
 
+
+
+    }
 }
 const std::string& List::lookup(size_t index) const {
-    std::string str = "";
-    return str;
-}
-void List::print(bool reverse = false) const {
+    Node* curr = head;
+    if (curr == NULL) {
+        throw std::out_of_range("out of range in lookup");
+    }
+    Node* next = curr->next;
 
+    size_t count = 0;
+    while (true) {
+        if (count == index-1) {
+            if (next==NULL) {
+                throw std::out_of_range("out of range in lookup");
+            }
+            else{
+                return next->data;
+            }
+        }
+        if (next == NULL) {
+            throw std::out_of_range("out of range in lookup");
+        }
+        curr = next;
+        next = next->next;
+        count++;
+    }
 }
+void List::print(bool reverse) const {
+    std::string *arr = new std::string[1000];
+    Node* curr = head;
+    int cnt = 0;
+    while (curr->next != NULL) {
+        if (cnt+1 == sizeof(arr)) {
+            std::string *temp = arr;
+            arr = new std::string[sizeof(arr)*2];
+            for(int i = 0; i<sizeof(temp);i++) {
+                arr[i] = temp[i];
+            }
+        }
+        arr[cnt] = curr->data;
+        curr = curr->next;
+        cnt++;
+    }
+    if (reverse) {
+        std::cout<<"["<<arr[cnt];
+        for (int i = cnt-1; i>=0;i--) {
+            std::cout<<", "<<arr[i];
+        }
+        std::cout<<"]"<<std::endl;
+    }
+    else {
+        std::cout<<"["<<arr[0];
+        for (int i = 1; i<=cnt;i--) {
+            std::cout<<", "<<arr[i];
+        }
+        std::cout<<"]"<<std::endl;
+    }
+    
+}
+
 
 std::string List::remove(size_t index) {
-    std::string str = "";
-    return str;
+    Node* curr = head;
+    if (curr == NULL) {
+        throw std::out_of_range("out of range in remove");
+    }
+    Node* next = curr->next;
+
+    size_t count = 0;
+    while (true) {
+        if (count == index-1) {
+            if (next==NULL) {
+                throw std::out_of_range("out of range in remove");
+            }
+            else{
+                std::string value = next->data;
+                curr->next = next->next;
+                delete next;
+                next = NULL;
+                return value;
+            }
+        }
+        if (next == NULL) {
+            throw std::out_of_range("out of range in remove");
+        }
+        curr = next;
+        next = next->next;
+        count++;
+    }
 }
 size_t List::remove(const std::string& value) {
-    return 0;                                   
+    Node* curr = head;
+    if (curr == NULL) {
+        return 0;
+    }
+    Node* next = curr->next;
+
+    size_t count = 0;
+    while (true) {
+        if (next==NULL) {
+            break;
+        }
+        else{
+            std::string value = next->data;
+            curr->next = next->next;
+            delete next;
+            next = NULL;
+            count++;
+        }
+        curr = next;
+        next = next->next;
+
+    }     
+    return count;                        
 }
