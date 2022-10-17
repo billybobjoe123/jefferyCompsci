c5ec8a1cc7420fb8de2b4ac0e24d111a85f587ff
jeffery wang <jeffery.wang.ca@gmail.com>
2022-10-16 23:13:41 -0700

    fixes

--- assort/List.cpp
+++ assort/List.cpp
@@ -221,7 +221,7 @@ size_t List::remove(const std::string& value) {
     size_t count = 0;
     while (true) {
         if(next==NULL) {
-            return count;
+            break;;
         }
         
         if (next->data == value) {
@@ -236,7 +236,7 @@ size_t List::remove(const std::string& value) {
             next = next->next;
         }
         else {
-            return;
+            break;
         }
     }     
     return count;                        
