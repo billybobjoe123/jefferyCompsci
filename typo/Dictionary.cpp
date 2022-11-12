557faf0a96f3c74d410d569da73c53327bc86e37
jeffery wang <jeffery.wang.ca@gmail.com>
2022-11-12 15:35:45 -0800

    first typo commit

---
+++ typo/Dictionary.cpp
@@ -0,0 +1,157 @@
+#include "Dictionary.h"
+#include <cmath>
+bool isLowerCase(std::string str) {
+    for(size_t i = 0;i<str.size();i++) {
+        if(isupper(str[i])) {
+            return false;
+        }
+    }
+    return true;
+
+}
+Point letterToPoint(char c) {
+    switch (c)
+    {
+    case 'a':
+        return QWERTY[0];
+        
+    case 'b':
+        /* code */
+        return QWERTY[1];
+        
+    case 'c':
+        /* code */
+        return QWERTY[2];
+        
+    case 'd':
+        /* code */
+        return QWERTY[3];
+        
+    case 'e':
+        /* code */
+        return QWERTY[4];
+        
+    case 'f':
+        /* code */
+        return QWERTY[5];
+        
+    case 'g':
+        /* code */
+        return QWERTY[6];
+        
+    case 'h':
+        /* code */
+        return QWERTY[7];
+        
+    case 'i':
+        /* code */
+        return QWERTY[8];
+        
+    case 'j':
+        /* code */
+        return QWERTY[9];
+        
+    case 'k':
+        /* code */
+        return QWERTY[10];
+        
+    case 'l':
+        /* code */
+        return QWERTY[11];
+        
+    
+    case 'm':
+        /* code */
+        return QWERTY[12];
+        
+    case 'n':
+        /* code */
+        return QWERTY[13];
+        
+    case 'o':
+        /* code */
+        return QWERTY[14];
+        
+    case 'p':
+        /* code */
+        return QWERTY[15];
+        
+    case 'q':
+        /* code */
+        return QWERTY[16];
+        
+    case 'r':
+        /* code */
+        return QWERTY[17];
+        
+    case 's':
+        /* code */
+        return QWERTY[18];
+        
+    case 't':
+        /* code */
+        return QWERTY[19];
+        
+    case 'u':
+        /* code */
+        return QWERTY[20];
+        
+    case 'v':
+        /* code */
+        return QWERTY[21];
+        
+    case 'w':
+        /* code */
+        return QWERTY[22];
+        
+    case 'x':
+        /* code */
+        return QWERTY[23];
+        
+    
+    case 'y':
+        /* code */
+        return QWERTY[24];
+        
+    case 'z':
+        /* code */
+        return QWERTY[25];
+        
+    }
+
+    
+}
+
+
+Dictionary::Dictionary(std::istream& stream) {
+    std::string line;
+    while(std::getline(stream,line)) {
+        if (line.length()>0 && line[0] != '#' && isLowerCase(line))  {
+            mWords.push_back(line);
+        }
+    }
+}
+
+Heap Dictionary::correct(const std::vector<Point>& points, size_t maxcount, float cutoff) const {
+    Heap wordHeap = Heap(maxcount);
+    for(size_t i = 0;i<mWords.size();i++) {
+        std::string word = mWords[i];
+        size_t wordSize = word.size();
+        float score = 0;
+        for(size_t i = 0;i<points.size();i++) {
+            if (i>=wordSize) {
+                break;
+            }
+            char c = word[i];
+            Point p = letterToPoint(c);
+            float dist = sqrt(pow(p.x - points[i].x,2) + pow(p.y - points[i].y,2));
+            score = score + 1 / (10*pow(dist,2) + 1);
+        }
+        score = score/points.size();
+        if(score >= cutoff) {
+            wordHeap.push(word,score);
+        }
+    }
+    return wordHeap;
+
+}
---
+++ typo/Heap.cpp
@@ -0,0 +1,172 @@
+#include "Heap.h"
+#include <stdexcept>
+Heap::Heap(size_t capacity) {
+    mData = new Entry[capacity];
+}
+
+Heap::Heap(const Heap& other) {
+    size_t count = other.count();
+    for(size_t i = 0; i < count; i++) {
+        Heap::Entry entry = other.lookup(i);
+        this->push(entry.value,entry.score);
+    }
+    this->mCapacity = other.capacity();
+    this->mCount = count;
+}
+Heap::Heap(Heap&& other) {
+    size_t count = other.count();
+    for(size_t i = 0; i < count; i++) {
+        Heap::Entry entry = other.pop();
+        this->push(entry.value,entry.score);
+    }
+    this->mCapacity = other.capacity();
+    this->mCount = count;
+}
+Heap::~Heap() {
+    delete[] this->mData;
+}
+size_t Heap::capacity() const {
+    return mCapacity;
+}
+size_t Heap::count() const {
+    return mCount;
+}
+
+const Heap::Entry& Heap::lookup(size_t index) const {
+    if (index<mCount) {
+        return mData[index];
+    }   
+    else {
+        throw std::out_of_range("out of range in lookup function");
+    }
+}
+
+Heap::Entry Heap::pop() {
+    if(mCount<=0) {
+        throw std::underflow_error("underflow in pop()");
+    }
+    Heap::Entry returnVal = mData[0];
+    if (mCount==1)  {
+        mCount--;
+        return returnVal;
+    }
+    mData[0] = mData[mCount-1];
+    mCount--;
+    size_t ind = 0;
+    while(ind<mCount) {
+        if (ind*2+1<mCount && ind*2+2<mCount) {
+            if (mData[ind].score>mData[ind*2+1].score && mData[ind*2+1].score<mData[ind*2+2].score) {
+                Heap::Entry temp = mData[ind*2+1];
+                mData[ind*2+1] = mData[ind];
+                mData[ind] = temp;
+                ind = ind*2+1;
+            }
+            else if(mData[ind].score>mData[ind*2+2].score && mData[ind*2+1].score>mData[ind*2+2].score) {
+                Heap::Entry temp = mData[ind*2+2];
+                mData[ind*2+2] = mData[ind];
+                mData[ind] = temp;
+                ind = ind*2+2;
+            }
+            else {
+                break;
+            }
+        }
+        else if(ind*2+1<mCount && ind*2+2>=mCount) {
+            if(mData[ind*2+1].score<mData[ind].score) {
+                Heap::Entry temp = mData[ind*2+1];
+                mData[ind*2+1] = mData[ind];
+                mData[ind] = temp;
+                ind = ind*2+1;
+            }
+            else {
+                break;
+            }
+        }
+        else {
+            break;
+        }
+    }
+    
+    return returnVal;   
+}
+
+Heap::Entry Heap::pushpop(const std::string& value, float score) {
+    if(mCount<=0) {
+        throw std::underflow_error("underflow in pushpop()");
+    }
+    Heap::Entry returnVal = mData[0];
+    Heap::Entry insertEntry;
+    insertEntry.score = score;
+    insertEntry.value = value;
+    
+    mData[0] = insertEntry; 
+    if (mCount==1){
+        return returnVal;
+    }
+    size_t ind = 0;
+    while(ind<mCount) {
+        if (ind*2+1<mCount && ind*2+2<mCount) {
+            if (mData[ind].score>mData[ind*2+1].score && mData[ind*2+1].score<mData[ind*2+2].score) {
+                Heap::Entry temp = mData[ind*2+1];
+                mData[ind*2+1] = mData[ind];
+                mData[ind] = temp;
+                ind = ind*2+1;
+            }
+            else if(mData[ind].score>mData[ind*2+2].score && mData[ind*2+1].score>mData[ind*2+2].score) {
+                Heap::Entry temp = mData[ind*2+2];
+                mData[ind*2+2] = mData[ind];
+                mData[ind] = temp;
+                ind = ind*2+2;
+            }
+            else {
+                break;
+            }
+        }
+        else if(ind*2+1<mCount && ind*2+2>=mCount) {
+            if(mData[ind*2+1].score<mData[ind].score) {
+                Heap::Entry temp = mData[ind*2+1];
+                mData[ind*2+1] = mData[ind];
+                mData[ind] = temp;
+                ind = ind*2+1;
+            }
+            else {
+                break;
+            }
+        }
+        else {
+            break;
+        }
+    }
+    return returnVal;
+}
+
+void Heap::push(const std::string& value, float score) {
+    if (mCapacity==mCount) {
+        throw std::overflow_error("Overflow in push()");
+    }
+    //insert
+    Heap::Entry insertEntry;
+    insertEntry.score = score;
+    insertEntry.value = value;
+    mData[mCount] = insertEntry;
+    //percolate up
+    size_t ind = mCount;
+    while(ind>=0) {
+        if((ind-1)/2>=0 && mData[ind].score>mData[(ind-1)/2].score) {
+            Heap::Entry temp = mData[(ind-1)/2];
+            mData[(ind-1)/2] = mData[ind];
+            mData[ind] = temp;
+            ind = (ind-1)/2;
+        }
+        else {
+            break;
+        }
+    }
+}
+
+const Heap::Entry& Heap::top() const {
+    if(mCount==0) {
+        throw std::underflow_error("underflow in top()");
+    }  
+    return mData[0];
+}
